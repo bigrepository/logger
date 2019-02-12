1d614b6c594c7d849959326f9a5d3fb62db38d14
BIGBYTES WEBSERVICES <github@bigbytes.at>
2019-02-12 14:37:41 +0100

    Create Logger.php

---
+++ Logger.php
@@ -0,0 +1,13 @@
+<?php
+namespace bigbytes\logger;
+
+  use Yii;
+  use yii\base\Component;
+	use yii\base\InvalidConfigException;
+ 
+	class Logger extends Component {
+ 
+ 		public function testiculi() {
+  		return "Logger Texticuli funktioniert";
+ 		}
+ }
